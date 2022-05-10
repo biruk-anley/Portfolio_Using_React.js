f4e332df6ee81c6d2b1432c1b77ff28b93269081
biruk <birukanley2022@gmail.com>
2022-05-10 14:33:43 +0300

    updating home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,51 @@
+import arrow from "./images/Group 5.svg";
+import church from "./images/Bitmap.jpg";
+import cloud from "./images/cloud.svg";
+import "../App.css";
 const Home = () => {
-    return ( 
-        <h2>Home Page</h2>
-     );
-}
- 
+  return (
+    // <div className="holder">
+    //   <div className="text">
+    //     <div className="up">
+    //       <div className="header">
+    //         <p>Travel</p>
+    //         <p>to</p>
+    //         <p>Ethiopia</p>
+    //       </div>
+
+    //       <div className="arrow">
+    //         <img src={arrow} alt="" />
+    //       </div>
+    //     </div>
+
+    //     <div className="down">
+    //       <p>
+    //         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum,
+    //         quis! Eos quis iusto ullam sint illo deleniti vero, quae amet
+    //         obcaecati, unde ducimus eligendi voluptatum aliquam doloribus
+    //         dolorum autem veniam.
+    //       </p>
+    //     </div>
+    //   </div>
+
+    //   <div className="container">
+    //     <div className="cloud">
+    //       <img src={cloud} alt="" />
+    //     </div>
+    //     <div className="church">
+    //       <img src={church} alt="" />
+    //     </div>
+    //   </div>
+    // </div>
+      <div className="holder">
+          <div className="one">
+              he
+          </div>
+          <div className="two">
+              two
+          </div>
+      </div>
+  );
+};
+
 export default Home;
