ed81ceff6cbde88a085b2094b8bbb3fae59df499
biruk <birukanley2022@gmail.com>
2022-05-11 14:43:03 +0300

    updating home content

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,50 +2,28 @@ import arrow from "./images/Group 5.svg";
 import church from "./images/Bitmap.jpg";
 import cloud from "./images/cloud.svg";
 import "../App.css";
+
+
 const Home = () => {
-  return (
-    // <div className="holder">
-    //   <div className="text">
-    //     <div className="up">
-    //       <div className="header">
-    //         <p>Travel</p>
-    //         <p>to</p>
-    //         <p>Ethiopia</p>
-    //       </div>
-
-    //       <div className="arrow">
-    //         <img src={arrow} alt="" />
-    //       </div>
-    //     </div>
-
-    //     <div className="down">
-    //       <p>
-    //         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum,
-    //         quis! Eos quis iusto ullam sint illo deleniti vero, quae amet
-    //         obcaecati, unde ducimus eligendi voluptatum aliquam doloribus
-    //         dolorum autem veniam.
-    //       </p>
-    //     </div>
-    //   </div>
-
-    //   <div className="container">
-    //     <div className="cloud">
-    //       <img src={cloud} alt="" />
-    //     </div>
-    //     <div className="church">
-    //       <img src={church} alt="" />
-    //     </div>
-    //   </div>
-    // </div>
-      <div className="holder">
-          <div className="one">
-              he
+  return ( 
+      <div className="Home-content">
+          {/* for content one */}
+          <div className="Home-content-one">
+              <h2>Travel</h2>
+              <p>To</p>
+              <img src={arrow}  alt="arrow pic" /> 
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quibusdam consectetur officiis dolore molestiae commodi.</p>
           </div>
-          <div className="two">
-              two
+
+
+          {/* for content Two*/}
+          <div className="Home-content-three">
+            <img src={cloud} alt="an image for the cloud" />
+            <img src={church} alt="an image for the church" />
+              
           </div>
       </div>
-  );
-};
+   );
+}
 
 export default Home;
