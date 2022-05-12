454100ebecd994f3c1d9452259a3bd66fea45e28
biruk <birukanley2022@gmail.com>
2022-05-13 00:46:38 +0300

    creating resource page

---
+++ src/pages/AboutComponent/Resource.js
@@ -0,0 +1,11 @@
+const Resource = () => {
+    return (
+      <div className="resource">
+        we collect those info from diffrent resources such as: እEthiopian
+        Orthodox tewahido churh web site wikepedia Mahibere kidusan web and
+        blogs Google Map yebetekrstian mereje by daniel kibret
+      </div>
+    );
+}
+ 
+export default Resource;
