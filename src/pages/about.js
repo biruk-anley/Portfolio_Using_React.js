e8ac91758056b580e021c7891ddf99c234a9bfee
biruk <birukanley2022@gmail.com>
2022-05-13 00:45:47 +0300

    updating eotc page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,27 @@
+import Advantage from "./AboutComponent/Advantage";
+import Resource from "./AboutComponent/Resource";
+import AboutUs from "./AboutComponent/AboutUs";
+// import { FaPhoneAlt } from "react-icons/fa";
+// import church from "./images/Bitmap.jpg";
+import Slider from "./Slider";
+
+
+
 const About = () => {
-    return ( 
-        <h2>here is about page</h2>
-     );
+    return (
+      <div className="about-holder">
+        <Slider/>
+        <ul>
+          <li>About us</li> 
+          <li>What can you get from us</li> 
+          <li>Resources</li>    
+        </ul>
+
+        <Advantage/>
+        <Resource/>
+        <AboutUs/>
+      </div>
+    );
 }
  
 export default About;
