02ec266f317190357065ef667ff5f15c7bb88e44
biruk <birukanley2022@gmail.com>
2022-05-13 00:46:59 +0300

    updating eotc

--- src/pages/eotc.js
+++ src/pages/eotc.js
@@ -1,6 +1,19 @@
+
+import Slider from "./Slider";
 const Eotc = () => {
     return ( 
-        <h2>eotc page</h2>
+        <div className="Eotc">
+            <Slider/>
+
+            <div className="content-1">
+                <h4>History</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, illum incidunt.</p>
+                <h4>Title 2</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, illum incidunt.</p>   
+                <h4>Title 3</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, illum incidunt.</p>
+            </div>
+        </div>
      );
 }
  
