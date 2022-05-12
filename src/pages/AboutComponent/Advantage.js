740b1007f976bbf135b26ce242bf1021ba18fff7
biruk <birukanley2022@gmail.com>
2022-05-13 00:46:49 +0300

    updagint eoct

---
+++ src/pages/AboutComponent/Advantage.js
@@ -0,0 +1,14 @@
+const Advantage = () => {
+    return (
+      <div className="holder">
+        <p>
+          when you use this website you will get : እestablishment and history of
+          the church how to get there religious schools of list down churches
+          some heritages miracles inside that curch it is supported with images
+          and videos
+        </p>
+      </div>
+    );
+}
+ 
+export default Advantage;
