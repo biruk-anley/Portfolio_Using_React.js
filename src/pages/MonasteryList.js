a6da424ebb3ec6dbfc5e148b8024b5fa4be5e52d
biruk <birukanley2022@gmail.com>
2022-05-14 12:37:10 +0300

    create monasterylist componenet

---
+++ src/pages/MonasteryList.js
@@ -0,0 +1,31 @@
+const MonasteryList = () => {
+    return ( 
+        <div className="Monastery-list">
+            <div className="img-holder">
+                <img src="" alt="" />
+            </div>
+
+            <div className="content">
+                <div className="udet">
+                    <p>April 5</p>
+                </div>
+                <div className="content-detail">
+                    <h2>Debre Tabor Medanialem, DebreTabor</h2>
+                    <hr />
+                    <p>It is one of historical church in  Ethipia which is first 
+                        built by Emperor Tewodros.It has historical heritage 
+                        It is known by spritual school called Digua, Metsihaft
+                        and others
+                    </p>
+                    <div className="buttons">
+                        <button>Go There</button>
+                        <button>Read More</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default MonasteryList;
