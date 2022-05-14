7115d1bdb2f8d96b4c473c7b51bf18ee340c66c7
biruk <birukanley2022@gmail.com>
2022-05-14 12:38:03 +0300

    creating heritage component

---
+++ src/pages/Components/Heritage.js
@@ -0,0 +1,12 @@
+const Heritage = () => {
+    return ( 
+        <div className="heritages">
+            <img src="" alt="" />
+            <h2>Heritage One</h2>
+            <hr />
+            <p>This is heritage that counts many year that hold ancient documents</p>
+        </div>
+     );
+}
+ 
+export default Heritage;
