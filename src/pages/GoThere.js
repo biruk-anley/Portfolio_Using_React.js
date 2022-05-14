1ab1fc2e3b6335771ee8031cf7c7dacccebabbe2
biruk <birukanley2022@gmail.com>
2022-05-14 12:36:04 +0300

    updating go there

---
+++ src/pages/GoThere.js
@@ -0,0 +1,16 @@
+const GoThere = () => {
+    return ( 
+        <div className="goto">
+            <Slider/>
+            <h2>About The Place</h2>
+            <li>Transoport : airplane</li>
+            <li>Environemnt : cold</li>
+            <h2>location:</h2>
+            <p>link from sheger to there</p>
+            <h2>Checkout map</h2>
+            <img src="" alt="" />
+        </div>
+     );
+}
+ 
+export default GoThere;
