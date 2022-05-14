52be9b74b80f5b5779ee0f41fb6d356b884fc96e
biruk <birukanley2022@gmail.com>
2022-05-14 12:35:54 +0300

    updating gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,6 +1,28 @@
+
+import church from "./images/Bitmap.jpg";
 const Gallery = () => {
     return ( 
-        <h2>Gallery page</h2>
+        <div className="gallerys">
+            <h2>Checkout Spritual Images</h2>
+            <div className="images-list">
+                <div className="list-one">
+                   <img src={church} alt=''/>
+                    <p>Lalibela</p>
+                </div>
+                <div className="list-one" alt=''>
+                   <img src={church} alt=''/>
+                    <p>Lalibela</p>
+                </div>
+                <div className="list-one">
+                   <img src={church} alt=''/>
+                    <p>Lalibela</p>
+                </div>
+                <div className="list-one">
+                   <img src={church} alt=''/>
+                    <p>Lalibela</p>
+                </div>
+            </div>
+        </div>
      );
 }
  
