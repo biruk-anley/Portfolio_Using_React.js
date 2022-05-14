bcf28c2ed11dde86ead0d724ae3d73335bd1a109
biruk <birukanley2022@gmail.com>
2022-05-14 12:36:46 +0300

    updating monastery

--- src/pages/monastery.js
+++ src/pages/monastery.js
@@ -1,7 +1,25 @@
+
+import MonasteryList from "./MonasteryList";
+import Slider from "./Slider";
 const Monastery = () => {
     return ( 
-        <h2>Monastery</h2>
+        <div className="Eotc">
+            <Slider/>
+            <h2>Historical and Miracle Full Orthodox Monasconst Monastery</h2>
+            <p>Ethiopia is blessed with historical and miracle full Monasconst Monasteryes that attracts everyone
+                in a Monasconst Monastery ther is priest , daiakon an chirtioans ,,,,,,the differnce between Monasconst Monastery and
+                monastery is
+            </p>
+            <div className="Monastery-list">
+                <MonasteryList/>
+                <MonasteryList/>
+                <MonasteryList/>
+                <MonasteryList/>
+            </div>
+        </div>
      );
 }
+ 
+
  
 export default Monastery;
