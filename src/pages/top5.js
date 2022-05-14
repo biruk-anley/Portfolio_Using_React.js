cc4fd6897310cd7e3251b36f71d29345cf32be32
biruk <birukanley2022@gmail.com>
2022-05-14 12:37:49 +0300

    updating top-5 component

--- src/pages/top5.js
+++ src/pages/top5.js
@@ -1,6 +1,29 @@
+import MonasteryList from "./MonasteryList";
+import Slider from "./Slider";
+
 const Top = () => {
     return ( 
-        <h2>Top 5 pages</h2>
+        <div className="top-selected">
+            <Slider/>
+            <h2>Top 5 Place to be visted</h2>
+            <p>why It is top 5 places reason for those top5
+                It will be selected based on niumber of rate
+            </p>
+            <div className="topPlace-holder">
+                <div className="list-content">
+                    <h2>1.DebreTabor Medanilaem</h2>
+                    <p>Peoples come to this place because of ....</p>
+                    <MonasteryList/>
+                </div>
+
+                <div className="list-content">
+                    <h2>2.Lalibela</h2>
+                    <p>Peoples come to this place because of ....</p>
+                    <MonasteryList/>
+                </div>
+            </div>
+
+        </div>
      );
 }
  
